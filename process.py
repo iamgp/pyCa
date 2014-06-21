1c26ac49f5d9e952904ccbdc61603db73ba49d42
Gareth Price <gareth.price@warwick.ac.uk>
2014-06-21 01:46:32 +0100

    removed yaml from process

--- process.py
+++ process.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
-import os, sys
-import pandas as pd
-import yaml
-
 from pyCa import *
 
 if __name__ == '__main__':
 
-	exp_folder = ''
-	treatment_folder = exp_folder + ''
+	exp_folder = '~/Dropbox/PhD/Data/Calcium Microfluorimetry/5. 5mM CM CM+ab M ~ 10uM/'
+	treatment_folder = exp_folder + 'Conditioned Media/'
 
 	scanDirAndSetUpExperiments(treatment_folder + 'settings.yaml')
--- pyCa/__init__.py
+++ pyCa/__init__.py
@@ -3,6 +3,7 @@ from Point import *
 from Stimulant import *
 from Cell import *
 from Experiment import *
+import yaml
 
 __version__ = 0.9
 __author__ = 'Gareth Price'
