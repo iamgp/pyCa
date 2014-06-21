20e7915424cde8804010186679b54eebfe90b27a
Gareth Price <gareth.price@warwick.ac.uk>
2014-06-21 02:14:51 +0100

    removed, process, added setup and binary

--- process.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from pyCa import *
-
-if __name__ == '__main__':
-
-	exp_folder = '~/Dropbox/PhD/Data/Calcium Microfluorimetry/5. 5mM CM CM+ab M ~ 10uM/'
-	treatment_folder = exp_folder + 'Conditioned Media/'
-
-	scanDirAndSetUpExperiments(treatment_folder + 'settings.yaml')
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pyCa',
+      version='1.0',
+      description='Calcium Microfluorimetry Analysis',
+      author='Gareth Price',
+      author_email='g@rhinoe.com',
+      packages=['pyCa'],
+      scripts=["bin/pyCa"]
+     )
